ba1cb0f59b1c9bec0924faa6f76702ba2cff27a4
jose manuel florez <jm.florezc@uniandes.edu.co>
2021-09-01 13:42:44 -0500

    All Authentication tests finished

--- src/test/java/Steps/Authentication.java
+++ src/test/java/Steps/Authentication.java
@@ -5,6 +5,7 @@ import io.restassured.response.Response;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 import org.hamcrest.Matchers.*;
 
@@ -24,10 +25,11 @@ public class Authentication {
     private static String password = "WsF6XRQ!r8QcZpW";
 
     private String requestToken = "";
+    private String session_id = "";
 
     @Test
     public void CreatingGuestSession(){
-        FachadaURL fachadaURL = new FachadaURL("https://api.themoviedb.org/3", "","authentication","","guest_session", true, ""+apiKey);
+        FachadaURL fachadaURL = new FachadaURL("https://api.themoviedb.org/3", "","authentication","","guest_session", true,false, ""+apiKey);
 
         String url = fachadaURL.construirURLFach();
 
@@ -38,7 +40,7 @@ public class Authentication {
 
     @Test
     public void CreatingRequestToken(){
-        FachadaURL fachadaURL = new FachadaURL("https://api.themoviedb.org/3", "","authentication","","token", true, ""+apiKey);
+        FachadaURL fachadaURL = new FachadaURL("https://api.themoviedb.org/3", "","authentication","","token", true,false, ""+apiKey);
 
         String url = fachadaURL.construirURLFach();
 
@@ -49,7 +51,7 @@ public class Authentication {
         response.then().extract().path("success").equals(true);
 
         if (response.then().extract().path("success").equals(true)){
-            requestToken = response.then().log().body().extract().path("request_token");
+            requestToken = response.then().extract().path("request_token");
             System.out.println(requestToken);
         }
 
@@ -58,7 +60,7 @@ public class Authentication {
 
     @Test
     public void CreatingSessionLoginIn(){
-        FachadaURL fachadaURL = new FachadaURL("https://api.themoviedb.org/3", "","authentication","","token", true, ""+apiKey);
+        FachadaURL fachadaURL = new FachadaURL("https://api.themoviedb.org/3", "","authentication","","token", true,false, ""+apiKey);
 
         String url = fachadaURL.construirURLFach();
 
@@ -68,28 +70,138 @@ public class Authentication {
         response.then().extract().path("success").equals(true);
 
         if (response.then().extract().path("success").equals(true)){
-            requestToken = response.then().log().body().extract().path("request_token");
-            System.out.println(requestToken);
+            requestToken = response.then().extract().path("request_token");
         }
 
         String jsonPayload = "{\n" +
                 " \"username\": \"" + username+ "\",\n" +
                 " \"password\": \"" + password+ "\",\n" +
-                " \"username\": \"" + requestToken+ "\"\n";
+                " \"request_token\": \"" + requestToken+ "\"\n}";
 
         System.out.println(jsonPayload);
 
-        FachadaURL fachadaURL2 = new FachadaURL("https://api.themoviedb.org/3", "","authentication","","token", true, ""+apiKey);
+        FachadaURL fachadaURL2 = new FachadaURL("https://api.themoviedb.org/3", "","authentication","","token", false,true , ""+apiKey);
 
-        String url2 = fachadaURL.construirURLFach();
+        String url2 = fachadaURL2.construirURLFach();
 
 
+        Response response2 = given().contentType("application/json").body(jsonPayload).when().post(url2);
+
+
+        response2.then().extract().path("success").equals(true);
+        response2.then().extract().path("request_token").equals(requestToken);
 
     }
 
     @Test
+    public void CreatingSession(){
+        FachadaURL fachadaURL = new FachadaURL("https://api.themoviedb.org/3", "","authentication","","token", true,false, ""+apiKey);
+
+        String url = fachadaURL.construirURLFach();
+
+        Response response = when().get(url);
+
+
+        response.then().extract().path("success").equals(true);
+
+        if (response.then().extract().path("success").equals(true)){
+            requestToken = response.then().extract().path("request_token");
+        }
+
+        String jsonPayload = "{\n" +
+                " \"username\": \"" + username+ "\",\n" +
+                " \"password\": \"" + password+ "\",\n" +
+                " \"request_token\": \"" + requestToken+ "\"\n}";
+
+
+        FachadaURL fachadaURL2 = new FachadaURL("https://api.themoviedb.org/3", "","authentication","","token", false,true , ""+apiKey);
+
+        String url2 = fachadaURL2.construirURLFach();
+
+
+        Response response2 = given().contentType("application/json").body(jsonPayload).when().post(url2);
+
+
+        response2.then().extract().path("success").equals(true);
+        response2.then().extract().path("request_token").equals(requestToken);
+
+        String jsonCreatingSession = "{\n" +
+                "\"request_token\": \"" + requestToken+ "\"\n}";
+
+        FachadaURL fachadaURL3 = new FachadaURL("https://api.themoviedb.org/3", "","authentication","","session", true,false, ""+apiKey);
+
+        String url3 = fachadaURL3.construirURLFach();
+
+        Response response3 = given().contentType("application/json").body(jsonCreatingSession).when().post(url3);
+
+        response3.then().extract().path("success").equals(true);
+        response3.then().extract().path("session_id").equals(Matchers.notNullValue());
+    }
+
+    @Test
+    public void DeletingExistingSession(){
+        FachadaURL fachadaURL = new FachadaURL("https://api.themoviedb.org/3", "","authentication","","token", true,false, ""+apiKey);
+
+        String url = fachadaURL.construirURLFach();
+
+        Response response = when().get(url);
+
+
+        response.then().extract().path("success").equals(true);
+
+        if (response.then().extract().path("success").equals(true)){
+            requestToken = response.then().extract().path("request_token");
+        }
+
+        String jsonPayload = "{\n" +
+                " \"username\": \"" + username+ "\",\n" +
+                " \"password\": \"" + password+ "\",\n" +
+                " \"request_token\": \"" + requestToken+ "\"\n}";
+
+        FachadaURL fachadaURL2 = new FachadaURL("https://api.themoviedb.org/3", "","authentication","","token", false,true , ""+apiKey);
+
+        String url2 = fachadaURL2.construirURLFach();
+
+
+        Response response2 = given().contentType("application/json").body(jsonPayload).when().post(url2);
+
+
+        response2.then().extract().path("success").equals(true);
+        response2.then().extract().path("request_token").equals(requestToken);
+
+        String jsonCreatingSession = "{\n" +
+                "\"request_token\": \"" + requestToken+ "\"\n}";
+
+        FachadaURL fachadaURL3 = new FachadaURL("https://api.themoviedb.org/3", "","authentication","","session", true,false, ""+apiKey);
+
+        String url3 = fachadaURL3.construirURLFach();
+
+        Response response3 = given().contentType("application/json").body(jsonCreatingSession).when().post(url3);
+
+        response3.then().extract().path("success").equals(true);
+        response3.then().extract().path("session_id").equals(Matchers.notNullValue());
+        if( response3.then().extract().path("session_id").equals(Matchers.notNullValue())){
+
+            session_id = response.jsonPath().getString("session_id");
+
+        }
+        FachadaURL fachadaURL4 = new FachadaURL("https://api.themoviedb.org/3", "","authentication","","session", false,false, ""+apiKey);
+
+        String url4 = fachadaURL4.construirURLFach();
+
+        String jsonDeletingSession = "{\n" +
+                "\"session_id\": \"" + session_id+ "\"\n}";
+
+        Response response4 = given().contentType("application/json").body(jsonDeletingSession).when().delete(url4);
+
+        response4.then().assertThat().extract().path("success").equals(true);
+
+    }
+
+
+    @Test
     public void getAlternativeTitles(){
-        FachadaURL fachadaURL = new FachadaURL("https://api.themoviedb.org/3", "","movie","619297-sweet-girl","alternative_titles",false, "2e172e18c00b19621224226c2fff60f9");
+        FachadaURL fachadaURL = new FachadaURL("https://api.themoviedb.org/3", "","movie","619297-sweet-girl","alternative_titles",false, false, "2e172e18c00b19621224226c2fff60f9");
 
         LOGGER.info("Starts running test getAlternativeTitles");
         String url = fachadaURL.construirURLFach();
@@ -102,7 +214,7 @@ public class Authentication {
     @Test
     public void getCreditsMovie(){
         //fachada es para steps
-        FachadaURL fachadaURL = new FachadaURL("https://api.themoviedb.org/3", "","movie","619297-sweet-girl","credits",false, "2e172e18c00b19621224226c2fff60f9");
+        FachadaURL fachadaURL = new FachadaURL("https://api.themoviedb.org/3", "","movie","619297-sweet-girl","credits",false, false,"2e172e18c00b19621224226c2fff60f9");
 
         LOGGER.info("Starts running test getAlternativeTitles");
         String url = fachadaURL.construirURLFach();
@@ -113,7 +225,7 @@ public class Authentication {
 
     @Test
     public void getCreditsTV(){
-        FachadaURL fachadaURL = new FachadaURL("https://api.themoviedb.org/3", "","tv","91363-what-if","credits",false , "2e172e18c00b19621224226c2fff60f9");
+        FachadaURL fachadaURL = new FachadaURL("https://api.themoviedb.org/3", "","tv","91363-what-if","credits",false , false,"2e172e18c00b19621224226c2fff60f9");
 
         LOGGER.info("Starts running test getCreditsTV");
         String url = fachadaURL.construirURLFach();
@@ -124,7 +236,7 @@ public class Authentication {
 
     @Test
     public void getEpisodeGroupsTV(){
-        FachadaURL fachadaURL = new FachadaURL("https://api.themoviedb.org/3", "","tv","91363-what-if","episode_groups",false,"2e172e18c00b19621224226c2fff60f9");
+        FachadaURL fachadaURL = new FachadaURL("https://api.themoviedb.org/3", "","tv","91363-what-if","episode_groups",false,false,"2e172e18c00b19621224226c2fff60f9");
 
         LOGGER.info("Empezo a correr el test getEpisodeGroupsTV");
         String url = fachadaURL.construirURLFach();
@@ -135,7 +247,7 @@ public class Authentication {
 
     @Test
     public void getPeopleDetail(){
-        FachadaURL fachadaURL = new FachadaURL("https://api.themoviedb.org/3", "","person","169337-katherine-lanasa","",false ,"2e172e18c00b19621224226c2fff60f9");
+        FachadaURL fachadaURL = new FachadaURL("https://api.themoviedb.org/3", "","person","169337-katherine-lanasa","",false , false,"2e172e18c00b19621224226c2fff60f9");
 
         LOGGER.info("Empezo a correr el test getPeopleDetail");
         String url = fachadaURL.construirURLFach();
